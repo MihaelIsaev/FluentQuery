00d9db3612934ce7d54ce64611d7361a684f0b53
Mihael Isaev <isaev.mihael@gmail.com>
2018-06-05 23:22:56 +0400

    FQWhere: implement group logic the right way ☝️

--- Sources/FluentQuery/FQWhere.swift
+++ Sources/FluentQuery/FQWhere.swift
@@ -13,21 +13,16 @@ public class FQWhere: FQPart {
         }
     }
     
-    public func groupStart() -> Self {
-        wheres.append("(")
-        return self
-    }
-    
-    public func groupEnd() -> Self {
-        wheres.append(")")
-        return self
-    }
-    
     public func and(_ predicates: FQPredicateGenericType...)  -> Self {
         for predicate in predicates {
             wheres.append("AND")
             wheres.append(predicate)
         }
+        return self
+    }
+    
+    public func and(_ inst: FQWhere)  -> Self {
+        joinAnotherInstance(inst, by: "AND")
         return self
     }
     
@@ -39,7 +34,24 @@ public class FQWhere: FQPart {
         return self
     }
     
+    public func or(_ inst: FQWhere)  -> Self {
+        joinAnotherInstance(inst, by: "OR")
+        return self
+    }
+    
     public var query: String {
         return wheres.map { $0.query }.joined(separator: " ")
+    }
+    
+    func joinAnotherInstance(_ inst: FQWhere, by: String) {
+        if inst.wheres.count > 0 {
+            wheres.append(by)
+            wheres.append("(")
+            for (index, predicate) in inst.wheres.enumerated() {
+                if index == 0, ["AND", "OR"].contains(predicate.query) { continue }
+                wheres.append(predicate)
+            }
+            wheres.append(")")
+        }
     }
 }
