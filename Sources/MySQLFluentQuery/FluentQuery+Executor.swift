8fea375fa8963e8dc8b11502d99c4f16e878a03e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-11 11:57:46 -0500

    Removed 'parameters' paramater from FluentQuery.execute MySQL methods

--- Sources/MySQLFluentQuery/FluentQuery+Executor.swift
+++ Sources/MySQLFluentQuery/FluentQuery+Executor.swift
@@ -2,17 +2,17 @@ import FluentMySQL
 @_exported import FluentQuery
 
 extension FluentQuery {
-    public func execute<D>(on conn: D, _ parameters: [MySQLDataConvertible] = []) -> Future<[[MySQLColumn : MySQLData]]> where D: MySQLConnection {
-        return conn.query(self.query, parameters)
+    public func execute<D>(on conn: D) -> Future<[[MySQLColumn : MySQLData]]> where D: MySQLConnection {
+        return conn.query(self.query, [])
     }
     
-    public func execute<D, T>(on conn: D, _ parameters: [MySQLDataConvertible] = [], andDecode to: T.Type, withDateDecodingStrategy strategy: JSONDecoder.DateDecodingStrategy? = nil)throws -> Future<[T]>
+    public func execute<D, T>(on conn: D, andDecode to: T.Type, withDateDecodingStrategy strategy: JSONDecoder.DateDecodingStrategy? = nil)throws -> Future<[T]>
         where D: MySQLConnection, T: Decodable {
-            return try conn.query(self.query, parameters).decode(T.self, dateDecodingStrategy: strategy)
+            return try conn.query(self.query, []).decode(T.self, dateDecodingStrategy: strategy)
     }
     
-    public func execute<D, T>(on conn: D, _ parameters: [MySQLDataConvertible] = [], andDecode to: [T].Type, withDateDecodingStrategy strategy: JSONDecoder.DateDecodingStrategy? = nil)throws -> Future<[T]>
+    public func execute<D, T>(on conn: D, andDecode to: [T].Type, withDateDecodingStrategy strategy: JSONDecoder.DateDecodingStrategy? = nil)throws -> Future<[T]>
         where D: MySQLConnection, T: Decodable {
-            return try conn.query(self.query, parameters).decode(T.self, dateDecodingStrategy: strategy)
+            return try conn.query(self.query, []).decode(T.self, dateDecodingStrategy: strategy)
     }
 }
