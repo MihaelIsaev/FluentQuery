d954d48e7fc6da7f484415283d099fbaf470d8ba
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-12 07:24:24 -0500

    Re-added date decoding strategy parameters to MySQL data decoders

--- Sources/MySQLFluentQuery/QueryDataDecoder+MySQL.swift
+++ Sources/MySQLFluentQuery/QueryDataDecoder+MySQL.swift
@@ -3,26 +3,26 @@ import FluentQuery
 import Foundation
 
 extension EventLoopFuture where T == [[MySQLColumn : MySQLData]] {
-    public func decode<T>(_ to: T.Type) throws -> EventLoopFuture<[T]> where T: Decodable {
-        return map { return try $0.decode(T.self) }
+    public func decode<T>(_ to: T.Type, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy? = nil) throws -> EventLoopFuture<[T]> where T: Decodable {
+        return map { return try $0.decode(T.self, dateDecodingStrategy: dateDecodingStrategy) }
     }
 }
 
 extension Array where Element == [MySQLColumn : MySQLData] {
-    public func decode<T>(_ to: T.Type) throws -> [T] where T: Decodable {
-        return try map { try $0.decode(T.self) }
+    public func decode<T>(_ to: T.Type, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy? = nil) throws -> [T] where T: Decodable {
+        return try map { try $0.decode(T.self, dateDecodingStrategy: dateDecodingStrategy) }
     }
 }
 
 extension Dictionary where Key == MySQL.MySQLColumn, Value == MySQL.MySQLData {
-    public func decode<T>(_ to: [T.Type]) throws -> T where T: Decodable {
-        return try decode(T.self)
+    public func decode<T>(_ to: [T.Type], dateDecodingStrategy: JSONDecoder.DateDecodingStrategy? = nil) throws -> T where T: Decodable {
+        return try decode(T.self, dateDecodingStrategy: dateDecodingStrategy)
     }
     
-    public func decode<T>(_ to: T.Type) throws -> T where T: Decodable {
+    public func decode<T>(_ to: T.Type, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy? = nil) throws -> T where T: Decodable {
         let convertedRowValues = map { (QueryField(name: $0.name), $1) }
         let convertedRow = Dictionary<QueryField, MySQL.MySQLData>(uniqueKeysWithValues: convertedRowValues)
-        return try FQDataDecoder(MySQLDatabase.self, entity: nil, dateDecodingStrategy: nil).decode(to, from: convertedRow)
+        return try FQDataDecoder(MySQLDatabase.self, entity: nil, dateDecodingStrategy: dateDecodingStrategy).decode(to, from: convertedRow)
     }
 }
 
