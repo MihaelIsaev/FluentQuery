0b5885b0fca6322c0635f08c11b18b073f2ba06a
Mihael Isaev <isaev.mihael@gmail.com>
2018-07-23 17:22:05 +0400

    Improve aliases by adding more convenient initializations

--- Sources/FluentQuery/FQAlias.swift
+++ Sources/FluentQuery/FQAlias.swift
@@ -1,5 +1,53 @@
 import Foundation
 import Fluent
+import Crypto
+
+extension Model {
+    /// Helper method for generating random string
+    private static func shuffledAlphabet(_ length: Int) -> String {
+        let letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        var randomString = ""
+        for _ in 0...length - 1 {
+            if let random = try? CryptoRandom().generate(UInt32.self) {
+                let rand = random % UInt32(letters.count)
+                let ind = Int(rand)
+                let character = letters[letters.index(letters.startIndex, offsetBy: ind)]
+                randomString.append(character)
+            }
+        }
+        return randomString
+    }
+    
+    /// It will return alias named: Model.name + randomString
+    /// it will return new alias every time you call it
+    public static var randomAlias: FQAlias<Self> {
+        return alias(shuffledAlphabet(3))
+    }
+    
+    /// It will return alias named: Model.name + number
+    public static func alias(_ number: Int) -> FQAlias<Self> {
+        return alias("\(number)")
+    }
+    
+    /// It will return alias named: Model.name + "Alias"
+    public static var alias: FQAlias<Self> {
+        return alias()
+    }
+    
+    /// By default it will return alias named: Model.name + "Alias"
+    /// as .alias lazy property
+    /// or you can provide your own string
+    /// and it will return alias named: Model.name + yourString
+    /// If you want to set your own short alias please use `alias(short:)` method
+    public static func alias(_ additionalString: String? = nil) -> FQAlias<Self> {
+        return FQAlias<Self>(entity + (additionalString ?? "Alias"))
+    }
+    
+    /// It will return alias named: your own string
+    public static func alias(short: String) -> FQAlias<Self> {
+        return FQAlias<Self>(short)
+    }
+}
 
 public class AliasedKeyPath<M, V>: FQPart where M: Model {
     var alias: String
