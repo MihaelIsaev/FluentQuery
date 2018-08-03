1495d8e8791bf52a0f825abe8104a84c5c966ff4
Mihael Isaev <isaev.mihael@gmail.com>
2018-08-03 04:59:06 +0400

    Implement extract function

--- Sources/FluentQuery/FQAggreagate.swift
+++ Sources/FluentQuery/FQAggreagate.swift
@@ -14,11 +14,14 @@ public class FQAggregate {
     
     //MARK: Original functions
     public enum Functions: FQAggregateFuncOption {
+        // Aggregate
         case count(String) //kp
         case sum(String) //kp
         case average(String) //kp
         case min(String) //kp
         case max(String) //kp
+        // Date/Time
+        case extract(ExtractField, ExtractType, String) //kp
         
         public var description: String {
             let description: String
@@ -33,6 +36,8 @@ public class FQAggregate {
                 description = "MIN\(FQAggregate.valueKey.roundBracketted)"
             case .max:
                 description = "MAX\(FQAggregate.valueKey.roundBracketted)"
+            case .extract:
+                description = "EXTRACT\(FQAggregate.valueKey.roundBracketted)"
             }
             return description
         }
@@ -54,6 +59,8 @@ public class FQAggregate {
                 value = v
             case let .max(v):
                 value = v
+            case let .extract(f, t, v):
+                value = f.rawValue+" FROM "+t.rawValue+" "+v
             }
             return value
         }
@@ -72,14 +79,16 @@ public class FQAggregate {
         case average(UKP)
         case min(UKP)
         case max(UKP)
+        case extract(ExtractField, ExtractType, UKP)
         
         var mirror: Functions {
             switch self {
-            case .count(let kp): return .count(kp.queryValue)
-            case .sum(let kp): return .sum(kp.queryValue)
-            case .average(let kp): return .average(kp.queryValue)
-            case .min(let kp): return .min(kp.queryValue)
-            case .max(let kp): return .max(kp.queryValue)
+            case let .count(kp): return .count(kp.queryValue)
+            case let .sum(kp): return .sum(kp.queryValue)
+            case let .average(kp): return .average(kp.queryValue)
+            case let .min(kp): return .min(kp.queryValue)
+            case let .max(kp): return .max(kp.queryValue)
+            case let .extract(f, t, kp): return .extract(f, t, kp.queryValue)
             }
         }
         
@@ -89,11 +98,12 @@ public class FQAggregate {
         
         var kp: UKP {
             switch self {
-            case .count(let kp): return kp
-            case .sum(let kp): return kp
-            case .average(let kp): return kp
-            case .min(let kp): return kp
-            case .max(let kp): return kp
+            case let .count(kp): return kp
+            case let .sum(kp): return kp
+            case let .average(kp): return kp
+            case let .min(kp): return kp
+            case let .max(kp): return kp
+            case let .extract(_, _, kp): return kp
             }
         }
         
@@ -117,6 +127,7 @@ public class FQAggregate {
         case average(FluentQuery)
         case min(FluentQuery)
         case max(FluentQuery)
+        case extract(ExtractField, ExtractType, FluentQuery)
         
         var mirror: Functions {
             switch self {
@@ -125,6 +136,7 @@ public class FQAggregate {
             case .average: return .average(String.roundBracketted(value))
             case .min: return .min(String.roundBracketted(value))
             case .max: return .max(String.roundBracketted(value))
+            case let .extract(f, t, _): return .extract(f, t, String.roundBracketted(value))
             }
         }
         
@@ -148,6 +160,8 @@ public class FQAggregate {
             case let .min(v):
                 value = v.query
             case let .max(v):
+                value = v.query
+            case let .extract(_, _, v):
                 value = v.query
             }
             return value
